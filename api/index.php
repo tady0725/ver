5f0cb101f64b8a55ca0f9ad12af07e329860f845
tady <78393880+tady0725@users.noreply.github.com>
2024-05-26 18:39:07 +0800

    0.1

--- api/index.php
+++ api/index.php
@@ -87,7 +87,7 @@
 
   </section><!-- End Hero Section -->
 
-  
+
 
   <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
@@ -95,7 +95,7 @@
 
   <!-- Vendor JS Files -->
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/aos/js/aos.js"></script>
   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
   <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
